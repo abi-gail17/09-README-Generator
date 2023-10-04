a5fdd9d664608f90767818cdb6ce85adb36ada46
Abigail <cinco.de.abby@gmail.com>
2023-10-03 23:42:22 -0400

    fixed some typos in the generate markdown, specifically some of the strings in the license functions

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -5,19 +5,19 @@ function renderLicenseBadge(license) {
     return '';
   }
   else if (license === 'Apache 2.0') {
-    return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+    return '[License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)';
   }
   else if (license === 'BSD 3-Clause') {
-    return '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+    return '[License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)';
   }
   else if (license === 'MIT') {
-    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    return '[License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
   }
   else if (license === 'Mozilla Public License 2.0') {
-    return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
+    return '[License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)'
   }
   else if (license === 'GNU GPL v3') {
-    return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+    return '[License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)'
   } 
 }
 
@@ -103,9 +103,7 @@ function generateMarkdown(data) {
 
   ## License
 
-  ${renderLicenseBadge(data.license)}
-  ${renderLicenseSection(data.license)}
-  ${renderLicenseLink(data.license)}
+  ${renderLicenseBadge(data.license)} ${renderLicenseSection(data.license)}: ${renderLicenseLink(data.license)}
 
   ## Contact Me
   Have any Questions? Contact me at:
